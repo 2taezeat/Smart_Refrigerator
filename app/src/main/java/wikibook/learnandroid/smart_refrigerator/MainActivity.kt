2f6d811b59a0782a39882463e48a13519ba6a7f7
2taezeat <xorudfl772@naver.com>
2021-10-31 17:53:56 +0900

    test commit

--- app/src/main/java/wikibook/learnandroid/smart_refrigerator/MainActivity.kt
+++ app/src/main/java/wikibook/learnandroid/smart_refrigerator/MainActivity.kt
@@ -1,16 +1,16 @@
 package wikibook.learnandroid.smart_refrigerator
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import wikibook.learnandroid.smart_refrigerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    // test 211031
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
