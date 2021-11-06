a3a6e46b1f0b09a568f7041567315c41a134c3e7
2taezeat <xorudfl772@naver.com>
2021-11-06 18:55:08 +0900

    feat : Camera, 사진 저장 기능

--- app/src/main/java/wikibook/learnandroid/smart_refrigerator/view/activity/MainActivity.kt
+++ app/src/main/java/wikibook/learnandroid/smart_refrigerator/view/activity/MainActivity.kt
@@ -1,28 +1,44 @@
 package wikibook.learnandroid.smart_refrigerator.view.activity
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import wikibook.learnandroid.smart_refrigerator.R
 import wikibook.learnandroid.smart_refrigerator.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    // test 211031
     private lateinit var binding: ActivityMainBinding
+
+    val REQUEST_IMAGE_CAPTURE = 1 // 카메라 사진 촬영 요청코드
+    lateinit var curPhotoPath : String // 문자열 형태의 사진 경로 값 ( 초기 값은 null )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each`
         // menu should be considered as top level destinations.
@@ -35,10 +51,107 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+
+
+        setPermission() // 권한을 체크하는 메소드
+
         binding.fab.setOnClickListener{
             Toast.makeText(this, "fab-test", Toast.LENGTH_SHORT).show()
+            takeCapture() // 기본 카메라 앱을 실행하여 사진 촬여
         }
 
-
     }
+
+    // 카메라 촬영
+    private fun takeCapture() {
+        // 기본 카메라 앱 실행
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try{
+                    createImageFile()
+                } catch (ex:IOException){
+                    null
+                }
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "wikibook.learnandroid.smart_refrigerator.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+
+
+    // 이미지 파일 생성
+    private fun createImageFile(): File? {
+        val timeStamp : String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir : File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("JPEG_${timeStamp}_", "jpg", storageDir)
+            .apply{
+                curPhotoPath = absolutePath
+            }
+    }
+
+
+    // ted permission setting
+    private fun setPermission() {
+        val permission = object : PermissionListener {
+            override fun onPermissionGranted() { // 설정해놓은 위험권한들이 허용 되었을 경우 수행됨
+                Toast.makeText(this@MainActivity, "권한이 허가되었습니다.", Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) { // 설정해놓은 위험권한들 중 거부를 한 경우 이곳을 수행함.
+                Toast.makeText(this@MainActivity, "권한이 허가되지 않았습니다", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        TedPermission.with(this)
+            .setPermissionListener(permission)
+            .setRationaleMessage("Please permit Camera permission")
+            .setDeniedMessage("Permission deny")
+            .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE,android.Manifest.permission.CAMERA)
+            .check()
+    }
+
+    // startActivityForResult를 통해서 기본 카메라 앱으로 부터 받아온 사진 결과 받아오
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) { // 이미지를 성공적으로 가져왔다면
+            val bitmap: Bitmap
+            val file = File(curPhotoPath)
+            if (Build.VERSION.SDK_INT < 28) { // 안드로이드 9.0 (Pie) 보다 낮은 경우
+                bitmap = MediaStore.Images.Media.getBitmap(contentResolver, Uri.fromFile(file))
+            } else {
+                val decode = ImageDecoder.createSource(
+                    this.contentResolver,
+                    Uri.fromFile(file)
+                )
+                bitmap = ImageDecoder.decodeBitmap(decode)
+            }
+            savePhoto(bitmap)
+        }
+    }
+
+    // 갤러리에 사진 저장
+   private fun savePhoto(bitmap: Bitmap) {
+        val folderPath = Environment.getExternalStorageDirectory().absolutePath + "/Pictures/" // 사진 폴더로 저장하기 위한 경로 선언
+        val timeStamp : String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val fileName = "${timeStamp}.jpeg"
+        val folder = File(folderPath)
+        if (!folder.isDirectory) { // 현재 해당 경로에 폴더가 존재하는지 검사, 존재하지 않는다
+            folder.mkdirs() // 해당 경로에 폴더 자동으로 새로 만들기
+        }
+
+        val out = FileOutputStream(folderPath + fileName)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+        Toast.makeText(this, "사진이 앨범에 저장되었습니다.", Toast.LENGTH_SHORT).show()
+   }
+
+
 }
